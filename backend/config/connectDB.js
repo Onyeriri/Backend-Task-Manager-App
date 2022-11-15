88456aec6be68588809852ff568d9c94c58916af
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-15 01:02:59 +0000

    Connected to database

---
+++ backend/config/connectDB.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(error);
+
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connectDB");
 
 const app = express();
 
@@ -7,4 +9,14 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const startSever = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startSever();
