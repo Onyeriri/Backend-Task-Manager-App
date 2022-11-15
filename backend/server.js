fabd785bcfd86d3ee29336c4d423258526f8926f
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-15 08:15:14 +0000

    Refactored server codes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,17 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
-const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("<h2>Welcome to home page...</h2>");
 });
 
-const PORT = process.env.PORT || 5000;
-
-const startSever = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-startSever();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err));
