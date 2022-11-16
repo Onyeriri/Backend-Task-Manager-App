2cb47a3ec5864840348f231ccbc449945d419d2a
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-16 19:58:34 +0000

    Added delete route to our api

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -15,7 +15,9 @@ const createTask = async (req, res) => {
 // get all task
 const getAllTask = async (req, res) => {
   try {
-    const task = await Task.find();
+    const task = await Task.find().sort({
+      createdAt: -1,
+    });
     res.status(200).json(task);
   } catch (error) {
     res.status(5000).json({ msg: error.message });
@@ -37,8 +39,23 @@ const getOneTask = async (req, res) => {
   }
 };
 
+// delete one task
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findOneAndDelete(id);
+
+    if (!task) {
+      return res.status(404).json("Id not found!!!");
+    }
+
+    res.status(200).json(task);
+  } catch (error) {}
+};
+
 module.exports = {
   createTask,
   getAllTask,
   getOneTask,
+  deleteTask,
 };
