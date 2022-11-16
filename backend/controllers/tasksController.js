304e3c4b479329aebe3c546803917a30ba3da518
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-16 21:46:09 +0000

    Added update route

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -49,8 +49,29 @@ const deleteTask = async (req, res) => {
       return res.status(404).json("Id not found!!!");
     }
 
-    res.status(200).json(task);
-  } catch (error) {}
+    res.status(200).send("Task delete");
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+// update a task
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json("Id not found!!!");
+    }
+
+    res.status(200).send("Task updated");
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
 module.exports = {
@@ -58,4 +79,5 @@ module.exports = {
   getAllTask,
   getOneTask,
   deleteTask,
+  updateTask,
 };
