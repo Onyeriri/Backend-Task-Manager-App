15c9313a002a21245ef2f116def19775bc224af8
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-16 22:16:20 +0000

    refactor routes

--- backend/routes/tasksRoute.js
+++ backend/routes/tasksRoute.js
@@ -9,10 +9,13 @@ const {
 } = require("../controllers/tasksController");
 const router = express.Router();
 
-router.post("/api/tasks", createTask);
-router.get("/api/tasks", getAllTask);
-router.get("/api/tasks/:id", getOneTask);
-router.delete("/api/tasks/:id", deleteTask);
-router.put("/api/tasks/:id", updateTask);
+router.route("/").post(createTask).get(getAllTask);
+
+router
+  .route("/:id")
+  .get(getOneTask)
+  .get(getAllTask)
+  .delete(deleteTask)
+  .put(updateTask);
 
 module.exports = router;
