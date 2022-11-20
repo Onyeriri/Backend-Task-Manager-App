4a7274ee7938b67a5aa469a4fb67b917adf53275
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-20 11:23:12 +0000

    Added get task count

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,15 +1,15 @@
 import { FaEdit, FaCheckDouble, FaRegTrashAlt } from "react-icons/fa";
 
-const Task = ({ task, index, handleDelete, getSingleTask }) => {
+const Task = ({ task, index, handleDelete, getSingleTask, taskCompleted }) => {
   let { name, _id } = task;
 
   return (
-    <div className="task">
+    <div className={task.completed ? "task completed" : "task"}>
       <p>
         <b>{index + 1}.</b> {name}
       </p>
       <div className="task-icons">
-        <FaCheckDouble color="green" />
+        <FaCheckDouble color="green" onClick={() => taskCompleted(task)} />
         <FaEdit color="purple" onClick={() => getSingleTask(task)} />
         <FaRegTrashAlt onClick={() => handleDelete(_id)} color="red" />
       </div>
